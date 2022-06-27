468a1c228879a9a89c912fd20fe7c12892dbc73e
ArtJDev <artemkurilenko1983@yandex.ru>
2022-06-27 13:16:42 +0300

    Commit 27.06.2022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         final LongAdder report = new LongAdder();
 
-        for (int i = 0; i < 3; i++) {
-            Thread stat = new Thread(new Shop(report));
-            stat.start();
-            stat.join();
-        }
+        Thread shop1 = new Thread(new Shop(report));
+        Thread shop2 = new Thread(new Shop(report));
+        Thread shop3 = new Thread(new Shop(report));
+        shop1.start();
+        shop2.start();
+        shop3.start();
+        shop1.join();
+        shop2.join();
+        shop3.join();
 
         System.out.println(report);
     }
--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,14 +1,13 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Shop implements Runnable {
     private final int VALUE = 12345;
     private final int DAYS = 30;
-    private final LongAdder stat;
+    private final LongAdder longAdder;
 
-    public Shop(LongAdder stat) {
-        this.stat = stat;
+    public Shop(LongAdder longAdder) {
+        this.longAdder = longAdder;
     }
 
     private void report(LongAdder longAdder) {
@@ -16,12 +15,12 @@ public class Shop implements Runnable {
         long[] days = new long[DAYS];
         for (int i = 0; i < days.length; i++) {
             days[i] = random.nextInt(VALUE);
+            longAdder.add(days[i]);
         }
-        longAdder.add(Arrays.stream(days).sum());
     }
 
     @Override
     public void run() {
-        report(stat);
+        report(longAdder);
     }
 }
